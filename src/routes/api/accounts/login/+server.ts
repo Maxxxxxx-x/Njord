d08e870a1717baa6aa87573a88b9adee448a166a
Maxxxxxx-x <maxxng1028@gmail.com>
2023-08-01 08:58:18 +0800

    logi napi

--- src/routes/api/accounts/login/+server.ts
+++ src/routes/api/accounts/login/+server.ts
@@ -5,12 +5,18 @@ import { error, json } from "@sveltejs/kit";
 import jwt from "@tsndr/cloudflare-worker-jwt";
 import { fromZodError } from "zod-validation-error";
 import type { RequestHandler } from "./$types";
+import { TimingSafeEqual } from "$lib/server/TimingSafeEqual";
 
 export const POST: RequestHandler = async ({ request, platform }) => {
     try {
         const { Username, Password } = LoginRequest.parse(await request.json());
-        console.log(Username, Password);
-        return json({ message: "Test" });
+        const AccessToken = jwt.sign({
+            iat: Date.now(),
+            exp: Math.floor(Date.now() / 1000) * (2 * (60 * 60)),
+        }, "SECRET", {
+            algorithm: "ES256"
+        });
+        return json({ access_token: AccessToken});
     } catch(err) {
         if (err instanceof z.ZodError) {
             throw error(400, fromZodError(err).message);
